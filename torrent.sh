2b1ab951eb02b9f0742033ce0f4553a2dccd0484
root <root@cicada.www.tendawifi.com>
2020-04-15 03:42:16 -0400

    first commit

---
+++ torrent.sh
@@ -0,0 +1,21 @@
+apt install figlet -y 
+
+figlet torrent
+
+sleep 4
+
+sudo add-apt-repository ppa:qbittorrent-team/qbittorrent-stable
+
+sudo apt-get install qbittorrent -y
+
+sudo apt --fix-broken install -y
+
+sudo apt-get install qbittorrent
+
+
+figlet finish
+
+sleep 3
+
+qbittorrent 
+
